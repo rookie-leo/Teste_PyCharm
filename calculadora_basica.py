a89dc9a4ddea5bf95806b784a55ec66a1e0b7809
rookie-leo <proleonardopa@gmail.com>
2020-05-13 13:13:51 -0300

    Continuando o teste com a primeira parte do codigo da calculadora simples.

---
+++ calculadora_basica.py
@@ -0,0 +1,10 @@
+def soma(x, z):
+    return x + z
+
+lang = str(input('Português/English? [P/E]')).upper()
+
+if lang in 'PPORTUGUÊSPORTUGUES':
+    x = int(input('Digite um número: '))
+
+elif lang in 'EENGLISH':
+    x = int(input('Type a number: '))
